450818c3a5ce95fe2754fd0ac69ffff83ec20e26
Giulio-Marra <giuliomarramail@gmail.com>
2025-12-17 11:10:16 +0100

    Creazione progetto springboot con dipendenze

---
+++ backend/src/main/java/giuliomarra/vinylvault/VinylvaultApplication.java
@@ -0,0 +1,13 @@
+package giuliomarra.vinylvault;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VinylvaultApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VinylvaultApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/giuliomarra/vinylvault/VinylvaultApplicationTests.java
@@ -0,0 +1,13 @@
+package giuliomarra.vinylvault;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VinylvaultApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

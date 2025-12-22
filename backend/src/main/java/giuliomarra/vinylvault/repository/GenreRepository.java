ea68cf9df30a50e68c92457cb76d3cc35ce8a2dc
Giulio-Marra <giuliomarramail@gmail.com>
2025-12-22 15:22:58 +0100

    Service e controller per le operazione CRUD sui generi

---
+++ backend/src/main/java/giuliomarra/vinylvault/repository/GenreRepository.java
@@ -0,0 +1,11 @@
+package giuliomarra.vinylvault.repository;
+
+
+import giuliomarra.vinylvault.model.Genre;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface GenreRepository extends JpaRepository<Genre, Long> {
+    boolean existsByName(String name);
+}

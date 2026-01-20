16ff9f87befbdf51a68e9c1a9ef2dcd3b73e23cd
Giulio-Marra <giuliomarramail@gmail.com>
2026-01-20 11:18:54 +0100

    Foter responsive per mobile web

--- web/src/shared/components/MyNavbar.jsx
+++ web/src/shared/components/MyNavbar.jsx
@@ -1,7 +1,14 @@
 import React, { useState } from "react";
-import { Button } from "react-bootstrap";
-import { FaShoppingCart, FaUser } from "react-icons/fa";
-import { FaMagnifyingGlass } from "react-icons/fa6";
+import {
+  Container,
+  Nav,
+  Navbar,
+  Form,
+  FormControl,
+  Button,
+} from "react-bootstrap";
+import { FaShoppingCart } from "react-icons/fa";
+import { FaUser, FaMagnifyingGlass } from "react-icons/fa6";
 import { Link, useNavigate } from "react-router";
 
 const MyNavbar = () => {
@@ -14,35 +21,48 @@ const MyNavbar = () => {
   };
 
   return (
-    <div className="navBar">
-      <div className="navBarSectionLogo">
-        <h2>Vinyl Vault</h2>
-        <Link className="navLink" to={"/catalog"}>
-          Shop All
-        </Link>
-        <Link className="navLink">New Arrivals </Link>
-        <Link className="navLink">Sale </Link>
-      </div>
-      <div className="navBarSectionRight">
-        <form onSubmit={handleSubmit}>
-          <div className="searchBarNav">
-            <FaMagnifyingGlass />
-            <input
-              value={query}
-              type="text"
-              placeholder="Search Artist, Album..."
-              onChange={(e) => setQuery(e.target.value)}
-            />
+    <Navbar expand="lg" className="navBar">
+      <Container fluid>
+        <Navbar.Brand as={Link} to="/" className="text-white fw-bold">
+          Vinyl Vault
+        </Navbar.Brand>
+
+        <Navbar.Toggle aria-controls="navbar-nav" className="border-0">
+          <span className="navbar-toggler-icon-custom"></span>
+        </Navbar.Toggle>
+
+        <Navbar.Collapse id="navbar-nav">
+          <Nav className="me-auto">
+            <Nav.Link as={Link} to="/catalog" className="navLink">
+              Shop All
+            </Nav.Link>
+            <Nav.Link className="navLink">New Arrivals</Nav.Link>
+            <Nav.Link className="navLink">Sale</Nav.Link>
+          </Nav>
+
+          <div className="d-flex align-items-center gap-2 mt-3 mt-lg-0">
+            <Form onSubmit={handleSubmit} className="searchBarNav">
+              <FaMagnifyingGlass />
+              <FormControl
+                type="text"
+                placeholder="Search Artist, Album..."
+                value={query}
+                onChange={(e) => setQuery(e.target.value)}
+                className="search-input"
+              />
+            </Form>
+
+            <Button className="btnNavBar">
+              <FaUser />
+            </Button>
+
+            <Button className="btnNavBar">
+              <FaShoppingCart />
+            </Button>
           </div>
-        </form>
-        <button className="btnNavBar">
-          <FaUser />
-        </button>
-        <button className="btnNavBar">
-          <FaShoppingCart />
-        </button>
-      </div>
-    </div>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
   );
 };
 

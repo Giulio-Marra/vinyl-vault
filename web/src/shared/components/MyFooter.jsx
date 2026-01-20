5498d6590136c62487f796fe2e664c82129b3686
Giulio-Marra <giuliomarramail@gmail.com>
2026-01-20 11:06:24 +0100

    Footer Responsive per mobile e tablet da web

--- web/src/shared/components/MyFooter.jsx
+++ web/src/shared/components/MyFooter.jsx
@@ -1,44 +1,61 @@
 import React from "react";
 import { SlSocialInstagram, SlSocialTwitter } from "react-icons/sl";
-import { Link } from "react-router";
+import { Link } from "react-router-dom";
+import { Container, Row, Col } from "react-bootstrap";
 
 const MyFooter = () => {
   return (
     <>
-      <div className="footerContainer">
-        <div>
-          <h3>Shop</h3>
-          <Link to="/products">Products</Link>
-          <Link to="/collections">Collections</Link>
-          <Link to="/new">New Arrivals</Link>
-          <Link to="/sale">Sale</Link>
-        </div>
-        <div>
-          <h3>Support</h3>
-          <Link to="/help">Help Center</Link>
-          <Link to="/faq">FAQ</Link>
-          <Link to="/shipping">Shipping</Link>
-          <Link to="/returns">Returns</Link>
-        </div>
-        <div>
-          <h3>Company</h3>
-          <Link to="/about">About Us</Link>
-          <Link to="/careers">Careers</Link>
-          <Link to="/blog">Blog</Link>
-          <Link to="/contact">Contact</Link>
-        </div>
-        <div className="colLinkFooter">
-          <h3>Follow Us</h3>
-          <Link to="https://instagram.com" target="_blank">
-            <SlSocialInstagram />
-          </Link>
-          <Link to="https://twitter.com" target="_blank">
-            <SlSocialTwitter />
-          </Link>
-        </div>
-      </div>
-      <div className="footerCopyright">
-        <p>© 2024 VinylVault. All rights reserved.</p>
+      <footer className="footerContainer py-5">
+        <Container>
+          <Row className="gy-4">
+            <Col xs={12} sm={6} md={3}>
+              <h3>Shop</h3>
+              <div className="d-flex flex-column gap-2">
+                <Link to="/products">Products</Link>
+                <Link to="/collections">Collections</Link>
+                <Link to="/new">New Arrivals</Link>
+                <Link to="/sale">Sale</Link>
+              </div>
+            </Col>
+
+            <Col xs={12} sm={6} md={3}>
+              <h3>Support</h3>
+              <div className="d-flex flex-column gap-2">
+                <Link to="/help">Help Center</Link>
+                <Link to="/faq">FAQ</Link>
+                <Link to="/shipping">Shipping</Link>
+                <Link to="/returns">Returns</Link>
+              </div>
+            </Col>
+
+            <Col xs={12} sm={6} md={3}>
+              <h3>Company</h3>
+              <div className="d-flex flex-column gap-2">
+                <Link to="/about">About Us</Link>
+                <Link to="/careers">Careers</Link>
+                <Link to="/blog">Blog</Link>
+                <Link to="/contact">Contact</Link>
+              </div>
+            </Col>
+
+            <Col xs={12} sm={6} md={3}>
+              <h3>Follow Us</h3>
+              <div className="d-flex gap-3 fs-4">
+                <Link to="https://instagram.com" target="_blank">
+                  <SlSocialInstagram />
+                </Link>
+                <Link to="https://twitter.com" target="_blank">
+                  <SlSocialTwitter />
+                </Link>
+              </div>
+            </Col>
+          </Row>
+        </Container>
+      </footer>
+
+      <div className="footerCopyright text-center py-3">
+        <p className="m-0">© 2024 VinylVault. All rights reserved.</p>
       </div>
     </>
   );
